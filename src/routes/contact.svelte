5494d85cb03d02eec1fb8fb05bf6b683489f0bd8
Ruairidh Williamson <williamsonruairidh@gmail.com>
2023-02-05 12:07:31 +0000

    Added email address

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -14,6 +14,8 @@
 
 <h1>Contact Me</h1>
 
+<h3>By email <a href="mailto:contact@rtaw.co.uk">contact@rtaw.co.uk</a></h3>
+
 <h3>Using the form ( <a href="privacy">privacy</a> )</h3>
 
 <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdDZ1cC3dVc_QlJibT4GlpTJybziQ9yjZiBl1TpoQ01jP7CNQ/viewform?embedded=true" title="Contact Form" frameborder=0 marginHeight=0 marginWidth=0></iframe>
